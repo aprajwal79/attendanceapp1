26cfb2db3b8dad6fec96c7a42a83eb1e1bfb4d2d
aprajwal79 <aprajwal79@gmail.com>
2023-03-24 20:49:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/ttv/facerecog/FaceRecognitionActivity.java
@@ -0,0 +1,14 @@
+package com.ttv.facerecog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FaceRecognitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_recognition);
+    }
+}
